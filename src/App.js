887ee0a4ccd16f6ee144759f6f5782eadad5a262
sumit <sumitstha03@gmail.com>
2024-02-04 09:19:46 +0545

    reacts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
+import './assets/css/Header.css';
+import {paraStyle,btnStyle} from "./Paragraph.js"
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import girlImage from "./assets/images/research.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* REACT HAS CAMEL CASE */}
+    {/* INLINE CSS */}
+      {/* <header style={{color:"blue", backgroundColor:"wheat"}}>sumit</header> */}
+
+      {/* EXTERNAL CSS */}
+      <header >sumit</header>
+
+      {/* SEPERATED STYLE OBJECT FILE CSS */}
+      <p style={paraStyle}>This is react course</p>
+      <button style={btnStyle}>Click me!</button>
+
+{/* BOOTSTRAP */}
+    <button className="btn btn-primary">bootstrap button</button>
+
+<img src={girlImage} alt="girl" style={{height:"200px", width:"200px"}}/>
+
+    </>
   );
 }
 
---
+++ src/Paragraph.js
@@ -0,0 +1,13 @@
+const paraStyle={
+    color:"black",
+    backgroundColor:"green",
+};
+
+const btnStyle={
+    color:"grey",
+    width:"100px",
+    backgroundColor:"white",
+    borderRadius:"20%",
+    padding:"20px",
+}
+export {paraStyle,btnStyle};
